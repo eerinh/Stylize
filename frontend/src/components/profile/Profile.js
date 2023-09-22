84e37e10fe8c1aabd14652bb6b61fb651fb15ff4
eerinh <sht2316@autuni.ac.nz>
2023-09-22 14:29:48 +1200

    Adding Star rating Icons to profile

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Navbar from '../browsing/Navbar';
-import './Profile.css'; // Import the required files
+import './Profile.css';
+import { FaStar } from "react-icons/fa";
 
 function Profile() {
+  const [userRating, setUserRating] = useState(null);
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+
+  const handleRatingChange = (ratingValue) => {
+    setUserRating(ratingValue);
+  };
+
+  const handleRatingClick = (ratingValue) => {
+    setRating(ratingValue);
+    handleRatingChange(ratingValue);
+  };
+
   return (
     <div className='profile-nav'>
       <Navbar />
-    
-    <div className='profile-info'>
-              <center><h1>Profile</h1></center>
+      <div className='profile-info'>
+        <center><h1>Profile</h1></center>
+      </div>
+      <div>
+        {[...Array(5)].map((star, index) => {
+          const ratingValue = index + 1;
 
+          return (
+            <label key={index}>
+              <input type="radio" name="rating" onClick={() => handleRatingClick(ratingValue)} />
+              <FaStar
+                className="star"
+                color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                size={75}
+                onMouseEnter={() => setHover(ratingValue)}
+                onMouseLeave={() => setHover(null)}
+              />
+            </label>
+          );
+        })}
+      </div>
     </div>
-    </div>
-
   );
 }
 
