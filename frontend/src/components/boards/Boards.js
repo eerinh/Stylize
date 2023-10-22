f01c7d990a6808cc94256c04e93b9a4d6e11dd4b
erin <sht2316@autuni.ac.nz>
2023-10-23 10:12:50 +1300

    Adding boards

--- frontend/src/components/boards/Boards.js
+++ frontend/src/components/boards/Boards.js
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../navbar/Navbar';
-import './Boards.css'; // Import the required files
+import './Boards.css';
 
 function Boards() {
+  //adding the boards 
+  const [panels, setPanels] = useState([
+    { title: "Enter Board Name Here" },
+    { title: "Enter Board Name Here" },
+    { title: "Enter Board Name Here" },
+    { title: "Enter Board Name Here" },
+  ]);
+
+
+  //allow users to edit the name of their board
+  const [editableBoardIndex, setEditableBoardIndex] = useState(); 
+  const [editedBoardName, setEditedBoardName] = useState('');
+
+  const handleEdit = (index, title) => {
+    setEditableBoardIndex(index);
+    setEditedBoardName(title);
+  };
+
+  const handleSave = (index) => {
+    const updatedPanels = [...panels];
+    updatedPanels[index].title = editedBoardName;
+    setPanels(updatedPanels);
+    setEditableBoardIndex(-1);
+  };
+
   return (
-    <div className='board-nav'>
+    <div className="board-nav">
       <Navbar />
-      <center><h1>Your Boards</h1></center>
+      <center>
+        <h1>Your Boards</h1>
+        <div className="panels">
+          {panels.map((panel, index) => (
+            <div key={index} className="panel">
+              {editableBoardIndex === index ? (
+                <div>
+                  <input
+                    type="text"
+                    value={editedBoardName}
+                    onChange={(e) => setEditedBoardName(e.target.value)}
+                  />
+                  <button onClick={() => handleSave(index)}>Save</button>
+                </div>
+              ) : (
+                <div>
+                  <span onClick={() => handleEdit(index, panel.title)}>
+                    {panel.title}
+                  </span>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </center>
     </div>
   );
 }
