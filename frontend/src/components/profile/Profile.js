4e0ed59ec2bb556500fe0b87a85df6931bc409e9
erin <sht2316@autuni.ac.nz>
2023-10-24 12:43:34 +1300

    Placeholder

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,8 +1,16 @@
-// import React, { useState } from 'react';
-// import Navbar from '../navbar/Navbar';
-// import './Profile.css';
-// import { FaStar } from "react-icons/fa"; //importing requried files
+import React, { useState } from 'react';
+import Navbar from '../navbar/Navbar';
+import './Profile.css';
 
+const Profile = () => {
+  return (
+    <div className="profile">
+      <Navbar />
+      <div className="profile-content">
+        <h2>User Profile</h2>
+      </div>
+    </div>
+  );
+};
 
-
-// export default Profile;
+export default Profile;
