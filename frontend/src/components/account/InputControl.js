7d22cc7fa3e803a946178eb229b09bb33980f236
s0sanp <celinesphilip@gmail.com>
2023-10-24 19:09:41 +1300

    Fixing error and adding comments

--- frontend/src/components/account/InputControl.js
+++ frontend/src/components/account/InputControl.js
@@ -3,9 +3,9 @@ import styles from './InputControl.module.css'; //import CSS module for styling
 
 function InputControl(props){
     return(
-        <div className={styles.container}> {/create container div with a class name from the CSS module/}
-            {props.label && <label>{props.label}</label>} {/If a label prop is provided, render label element/}
-            <input type="text"{...props}/> {/Render an input element with type 'text' and spread the props/}
+        <div className={styles.container}> {/*create container div with a class name from the CSS module*/}
+            {props.label && <label>{props.label}</label>} {/*If a label prop is provided, render label element*/}
+            <input type="text"{...props}/> {/*Render an input element with type 'text' and spread the props*/}
         </div>
     );
 }
--- frontend/src/components/account/Signup.js
+++ frontend/src/components/account/Signup.js
@@ -1,53 +1,155 @@
-import React, { useState } from 'react' //import navigate and useState hook
-import { Link, useNavigate } from "react-router-dom"; //import link and useNavigate
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'; //import functions fom firebase/auth library
+// import React, { useState } from 'react' //import navigate and useState hook
+// import { Link, useNavigate } from "react-router-dom"; //import link and useNavigate
+// import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'; //import functions fom firebase/auth library
 
-import InputControl from './InputControl'; //import input control component
-import { auth } from '../database'; //import auth object from the .database file
+// import InputControl from './InputControl'; //import input control component
+// import { auth } from '../database'; //import auth object from the .database file
 
-//import stylesheet
+// //import stylesheet
+// import styles from './Signup.module.css'
+
+// function Signup() {
+//     const navigate = useNavigate(); //get navigate function from the router
+//     const [values, setValues] = useState({
+//         name: "",
+//         email: "",
+//         pass: "",
+//     }); //initialise state variables for user input values
+
+//     const [errorMsg, setErrorMsg] = useState(""); //initialise state variable for error messages
+//     const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false); //initialise state for disabling submit
+//     const [showPassword, setShowPassword] = useState(false); //initialise state variable for showing/hiding the password
+
+//     const handleSubmission = () => {
+//         if (!values.name || !values.email || !values.pass) {
+//             setErrorMsg("Please fill all fields"); //display an error if any of the fields are empty
+//             return;
+//         }
+//         setErrorMsg("");
+//         setSubmitButtonDisabled(true);
+
+//         createUserWithEmailAndPassword(auth, values.email, values.pass) //create new user with provided email and password
+//             .then(async (res) => {
+//                 setSubmitButtonDisabled(false);
+//                 const user = res.user;
+//                 await updateProfile(user, {
+//                     displayName: values.name,
+//                 }); //update user profile with the provided name
+//                 navigate("/following"); //navigate to the /following page if successful
+//             })
+//             .catch((err) => {
+//                 setSubmitButtonDisabled(false);
+//                 setErrorMsg(err.message); //display error if unsuccessful
+//             });
+//     };
+
+//     const togglePasswordVisibility = () => {
+//         setShowPassword(!showPassword); //toggle visibility of password either show/hide
+//     };
+
+//     const isPasswordEmpty = values.pass.trim() === ''; //check if password is empty after triming whitespace
+
+//     return (
+//         <div className={styles.container}>
+//             <div className={styles.innerBox}>
+//                 <h1 className={styles.heading}>Sign Up</h1>
+
+//                 <InputControl label="Name" placeholder="Enter your Full Name"
+//                     onChange={(event) =>
+//                         setValues((prev) => ({ ...prev, name: event.target.value }))
+//                     }
+//                 /> {/*Render input control for name with an event handler for updating the state*/}
+//                 <InputControl label="Email" placeholder="Enter your Email Address"
+//                     onChange={(event) =>
+//                         setValues((prev) => ({ ...prev, email: event.target.value }))
+//                     }
+//                 /> {/*Render an input control for the email*/}
+
+//                 <div className={styles.passwordInput}>
+//                     <InputControl label="Password" placeholder="Enter your Password"
+//                         type={showPassword ? 'text' : 'password'}
+//                         value={values.pass}
+//                         onChange={(event) =>
+//                             setValues((prev) => ({ ...prev, pass: event.target.value }))
+//                         }
+//                     /> {/*Render an input control for the password*/}
+
+//                     {!isPasswordEmpty && (
+//                         <button
+//                             className={styles.togglePasswordButton}
+//                             onClick={togglePasswordVisibility}
+//                         >
+//                             {showPassword ? 'Hide' : 'Show'}
+//                         </button>
+//                     )} {/*Render a button to toggle password visibility (show/hide) */}
+//                 </div>
+
+
+
+//                 <div className={styles.footer}>
+//                     <b className={styles.error}> {errorMsg}</b> {/**Display error message if there is any */}
+//                     <button onClick={handleSubmission} disabled={submitButtonDisabled}>Sign Up</button>
+//                     <p>
+//                         Already have an account?{" "}
+//                         <span>
+//                             <Link to="/login">Login</Link> {/*Redirect to the login page if user already has account*/}
+//                         </span>
+//                     </p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Signup; //export signup
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+
+import InputControl from './InputControl';
+import { auth } from '../database';
+
 import styles from './Signup.module.css'
 
 function Signup(){
-    const navigate = useNavigate(); //get navigate function from the router
+    const navigate = useNavigate();
     const[values, setValues]=useState({
         name: "",
         email: "",
         pass: "",
-    }); //initialise state variables for user input values
-
-    const [errorMsg, setErrorMsg] = useState(""); //initialise state variable for error messages
-    const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false); //initialise state for disabling submit
-    const [showPassword, setShowPassword] = useState(false); //initialise state variable for showing/hiding the password
+    });
+    const [errorMsg, setErrorMsg] = useState("");
+    const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleSubmission=() => {
         if(!values.name || !values.email || !values.pass){
-            setErrorMsg("Please fill all fields"); //display an error if any of the fields are empty
+            setErrorMsg("Please fill all fields");
             return;
         }
         setErrorMsg("");
         setSubmitButtonDisabled(true);
 
-        createUserWithEmailAndPassword(auth, values.email, values.pass) //create new user with provided email and password
+        createUserWithEmailAndPassword(auth, values.email, values.pass)
             .then(async(res) => {
                 setSubmitButtonDisabled(false);
                 const user =res.user;
                 await updateProfile(user, {
                     displayName: values.name,
-                }); //update user profile with the provided name
-                navigate("/following"); //navigate to the /following page if successful
+                });
+                navigate("/following");
             })
             .catch((err) => {
-                setSubmitButtonDisabled(false); 
-                setErrorMsg(err.message); //display error if unsuccessful
+                setSubmitButtonDisabled(false);
+                setErrorMsg(err.message);
             });
     };
     
     const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword); //toggle visibility of password either show/hide
+        setShowPassword(!showPassword);
     };
 
-    const isPasswordEmpty = values.pass.trim() ===''; //check if password is empty after triming whitespace
+    const isPasswordEmpty = values.pass.trim() ==='';
     
     return(
         <div className={styles.container}>
@@ -58,21 +160,21 @@ function Signup(){
                     onChange={(event) =>
                         setValues((prev) => ({...prev, name: event.target.value }))
                 }
-                /> {/*Render input control for name with an event handler for updating the state*/}
+                />
                 <InputControl label="Email" placeholder="Enter your Email Address"
                     onChange={(event) =>
                         setValues((prev) => ({...prev, email: event.target.value }))
                 }
-                /> {/*Render an input control for the email*/}
+                />
 
                 <div className={styles.passwordInput}>
                     <InputControl label="Password" placeholder="Enter your Password"
-                        type={showPassword ? 'text' : 'password'}
+                       type={showPassword ? 'text' : 'password'}
                         value={values.pass}
                         onChange={(event) =>
                             setValues((prev) => ({...prev, pass: event.target.value }))
                         }   
-                    /> {/*Render an input control for the password*/}
+                    /> 
 
                     {!isPasswordEmpty && (
                         <button
@@ -81,18 +183,18 @@ function Signup(){
                         >
                             {showPassword ? 'Hide' : 'Show'}
                         </button>
-                    )} {/*Render a button to toggle password visibility (show/hide) */}
+                    )}
                 </div>
 
                 
 
                 <div className={styles.footer}>
-                    <b className={styles.error}> {errorMsg}</b> {/**Display error message if there is any */}
+                    <b className={styles.error}> {errorMsg}</b>
                     <button onClick={handleSubmission} disabled = {submitButtonDisabled}>Sign Up</button>
                     <p>
                         Already have an account?{" "} 
                         <span>
-                            <Link to="/login">Login</Link> {/*Redirect to the login page if user already has account*/}
+                            <Link to="/login">Login</Link>
                         </span>
                     </p>
                 </div>
@@ -101,4 +203,4 @@ function Signup(){
     );
 }
 
-export default Signup; //export signup
+export default Signup;
