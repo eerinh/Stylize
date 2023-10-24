862776a614c563a844e3223f8d2f8c1de829a464
s0sanp <celinesphilip@gmail.com>
2023-10-24 18:48:49 +1300

    Updating my comments for account feature.

--- frontend/src/components/account/InputControl.js
+++ frontend/src/components/account/InputControl.js
@@ -1,14 +1,13 @@
-import React from 'react';
-
-import styles from './InputControl.module.css';
+import React from 'react'; //import react library
+import styles from './InputControl.module.css'; //import CSS module for styling
 
 function InputControl(props){
     return(
-        <div className={styles.container}>
-            {props.label && <label>{props.label}</label>}
-            <input type="text"{...props}/>
+        <div className={styles.container}> {/create container div with a class name from the CSS module/}
+            {props.label && <label>{props.label}</label>} {/If a label prop is provided, render label element/}
+            <input type="text"{...props}/> {/Render an input element with type 'text' and spread the props/}
         </div>
     );
 }
 
-export default InputControl;
+export default InputControl; //export input control component so that we can use this in then rest of the component.
